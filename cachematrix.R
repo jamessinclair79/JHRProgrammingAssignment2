c3d10fa42190ecb321a51536ed87e717901df48e
jamessinclair79 <33604324+jamessinclair79@users.noreply.github.com>
2019-02-09 23:26:55 +0400

    updated

--- cachematrix.R
+++ cachematrix.R
@@ -33,20 +33,14 @@ cacheSolve <- function(x, ...) {
         m                                      ##returns value of inverse to the parent enironment.
 }
 
-=======
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
->>>>>>> 7f657dd22ac20d22698c53b23f0057e1a12c09b7
+======
+## x <- matrix(c(1/2, -1/4, -1, 3/4), nrow = 2, ncol = 2)
+## myMatrixObject <- makeCacheMatrix(x)
+## cacheSolve(myMatrixObject)
+## [,1] [,2]
+## [1,]    1    0
+## [2,]    0    1
+## x
+## [,1]  [,2]
+## [1,]  0.50 -1.00
+## [2,] -0.25  0.75
